3b92cee36685f074aa2f176255893dd8fc3707ae
FelixTellmann <felix@tellmann.co.za>
2020-10-21 19:04:48 +0200

    Eslint Fix

--- src/_utils.ts
+++ src/_utils.ts
@@ -1,60 +1,60 @@
-import { Border, Color, Flex, Grid, Margin, Other, Padding, Position, Size, Typography } from './css';
+import { Border, Color, Flex, Grid, Margin, Other, Padding, Position, Size, Typography } from "./css";
 
 export type ConfigProps = {
   Padding?: boolean;
   Margin?: boolean;
   Size?: boolean;
   Space?: boolean;
-
+  
   Position?: boolean;
   Flex?: boolean;
   Grid?: boolean;
   Layout?: boolean;
-
+  
   Border?: boolean;
   Color?: boolean;
   Typography?: boolean;
   Decor?: boolean;
-
+  
   Other?: boolean;
   All?: boolean;
-
+  
   remBase?: number;
   fontSizes?: number[];
   spaceSizes?: number[];
 };
 
 export const splitProps = (
-  props: unknown,
-  CssOptions = Object.keys({ ...Padding, ...Margin, ...Size, ...Position, ...Flex, ...Grid, ...Border, ...Color, ...Typography, ...Other })
+    props: unknown,
+    CssOptions = Object.keys({ ...Padding, ...Margin, ...Size, ...Position, ...Flex, ...Grid, ...Border, ...Color, ...Typography, ...Other })
 ) => {
   return Object.keys(props).reduce(
-    ({ cssProps, nonCssProps }, key) => {
-      if (CssOptions.includes(key)) {
+      ({ cssProps, nonCssProps }, key) => {
+        if (CssOptions.includes(key)) {
+          return {
+            cssProps: {
+              ...cssProps,
+              [key]: props[key]
+            },
+            nonCssProps: {
+              ...nonCssProps
+            }
+          };
+        }
         return {
           cssProps: {
-            ...cssProps,
-            [key]: props[key]
+            ...cssProps
           },
           nonCssProps: {
-            ...nonCssProps
+            ...nonCssProps,
+            [key]: props[key]
           }
         };
+      },
+      {
+        cssProps: {},
+        nonCssProps: {}
       }
-      return {
-        cssProps: {
-          ...cssProps
-        },
-        nonCssProps: {
-          ...nonCssProps,
-          [key]: props[key]
-        }
-      };
-    },
-    {
-      cssProps: {},
-      nonCssProps: {}
-    }
   );
 };
 
@@ -75,56 +75,56 @@ export const hasResponsiveProps = (props) => {
 };
 
 export const createStyledJsxStrings = (
-  props: unknown,
-  { remBase, fontSizes, spaceSizes, breakPointIndex, ...config }: ConfigProps & { breakPointIndex?: number }
+    props: unknown,
+    { remBase, fontSizes, spaceSizes, breakPointIndex, ...config }: ConfigProps & { breakPointIndex?: number }
 ) => {
   /*= =============== Load Options ================ */
-  let selectedCSSOptions:  unknown;
+  let selectedCSSOptions: unknown;
   if (isEmpty(config)) {
     selectedCSSOptions = { ...Padding, ...Margin, ...Size, ...Position, ...Flex, ...Grid, ...Border, ...Color, ...Typography, ...Other };
   }
   selectedCSSOptions = Object.entries(config).reduce((acc, [key, value]) => {
     if (!value) return acc;
-    if (key === 'Padding') {
+    if (key === "Padding") {
       acc = { ...acc, ...Padding };
     }
-    if (key === 'Margin') {
+    if (key === "Margin") {
       acc = { ...acc, ...Margin };
     }
-    if (key === 'Size') {
+    if (key === "Size") {
       acc = { ...acc, ...Size };
     }
-    if (key === 'Space') {
+    if (key === "Space") {
       acc = { ...acc, ...Padding, ...Margin, ...Size };
     }
-    if (key === 'Position') {
+    if (key === "Position") {
       acc = { ...acc, ...Position };
     }
-    if (key === 'Flex') {
+    if (key === "Flex") {
       acc = { ...acc, ...Flex };
     }
-    if (key === 'Grid') {
+    if (key === "Grid") {
       acc = { ...acc, ...Grid };
     }
-    if (key === 'Layout') {
+    if (key === "Layout") {
       acc = { ...acc, ...Position, ...Flex, ...Grid };
     }
-    if (key === 'Border') {
+    if (key === "Border") {
       acc = { ...acc, ...Border };
     }
-    if (key === 'Color') {
+    if (key === "Color") {
       acc = { ...acc, ...Color };
     }
-    if (key === 'Typography') {
+    if (key === "Typography") {
       acc = { ...acc, ...Typography };
     }
-    if (key === 'Decor') {
+    if (key === "Decor") {
       acc = { ...acc, ...Border, ...Color, ...Typography };
     }
-    if (key === 'other') {
+    if (key === "other") {
       acc = { ...acc, ...Other };
     }
-    if (key === 'All') {
+    if (key === "All") {
       acc = { ...acc, ...Padding, ...Margin, ...Size, ...Position, ...Flex, ...Grid, ...Border, ...Color, ...Typography, ...Other };
     }
     return acc;
@@ -132,8 +132,8 @@ export const createStyledJsxStrings = (
   const { cssProps } = splitProps(props, Object.keys(selectedCSSOptions));
   const toStringAndVariables = (value: string | number): string => value.toString().replace(/^--.+/, (match) => `var(${match})`);
   const convertValue = (key: string, value: number | string) => {
-    let converter = '';
-    if (selectedCSSOptions[key] === '') {
+    let converter = "";
+    if (selectedCSSOptions[key] === "") {
       return toStringAndVariables(value);
     }
     if (Array.isArray(selectedCSSOptions[key])) {
@@ -141,50 +141,49 @@ export const createStyledJsxStrings = (
     } else {
       converter = selectedCSSOptions[key];
     }
-
-    if (typeof value === 'number' && value >= 0 && value <= fontSizes.length && converter === 'fontSize') {
+    
+    if (typeof value === "number" && value >= 0 && value <= fontSizes.length && converter === "fontSize") {
       return `${fontSizes[value] / remBase}rem`;
     }
-
-    if (typeof value === 'number' && value >= 0 && value <= spaceSizes.length && converter === 'space') {
+    
+    if (typeof value === "number" && value >= 0 && value <= spaceSizes.length && converter === "space") {
       return `${spaceSizes[value] / remBase}rem`;
     }
-
-    if (typeof value === 'number' && value > 8) {
+    
+    if (typeof value === "number" && value > 8) {
       return `${value / remBase}rem`;
     }
-
-    if (typeof value === 'string') {
+    
+    if (typeof value === "string") {
       // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
-      return value.match(/(px)$/) ? `${+value.replace('px', '') / remBase}rem` : toStringAndVariables(value);
+      return value.match(/(px)$/) ? `${+value.replace("px", "") / remBase}rem` : toStringAndVariables(value);
     }
-
+    
     return toStringAndVariables(value);
   };
   const toCssProperty = (key, value): string => `${key.replace(/([A-Z])/g, (match) => `-${match.toLowerCase()}`)}: ${convertValue(key, value)};\n`;
   
-
   const expandToCssPropertyStrings = (key, value): string => {
     if (Array.isArray(selectedCSSOptions[key])) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return selectedCSSOptions[key].map((k) => toCssProperty(k === '' || k === 'space' || k === 'fontSize' ? key : k, value)).join('');
+      return selectedCSSOptions[key].map((k) => toCssProperty(k === "" || k === "space" || k === "fontSize" ? key : k, value)).join("");
     }
     return toCssProperty(key, value);
   };
-
-  return Object.entries(cssProps)
-    .reduce((acc: string[], [key, value]) => {
-      // check if responsive
-      if (Array.isArray(value) && typeof window !== 'undefined') {
-        acc.push(
-          expandToCssPropertyStrings(key, value[breakPointIndex] || value[breakPointIndex] === 0 ? value[breakPointIndex] : value[value.length - 1])
-        );
-        // if not responsive
-      } else {
-        acc.push(expandToCssPropertyStrings(key, Array.isArray(value) ? value[0] : value));
-      }
-
-      return acc;
-    }, [])
-    .join('');
+  
+  return Object.entries(cssProps).reduce((acc: string[], [key, value]) => {
+    // check if responsive
+    if (Array.isArray(value) && typeof window !== "undefined") {
+      acc.push(
+          expandToCssPropertyStrings(key, value[breakPointIndex] || value[breakPointIndex] === 0
+                                          ? value[breakPointIndex]
+                                          : value[value.length - 1])
+      );
+      // if not responsive
+    } else {
+      acc.push(expandToCssPropertyStrings(key, Array.isArray(value) ? value[0] : value));
+    }
+    
+    return acc;
+  }, []).join("");
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useEffect, useState } from 'react';
-import { ConfigProps, createStyledJsxStrings, splitProps } from './_utils';
+import React, { FC, useEffect, useState } from "react";
+import { ConfigProps, createStyledJsxStrings, splitProps } from "./_utils";
 import {
   BorderProperties,
   ColorProperties,
@@ -11,7 +11,7 @@ import {
   PositionProperties,
   SizeProperties,
   TypographyProperties
-} from './css';
+} from "./css";
 
 export {
   BorderProperties,
@@ -40,24 +40,27 @@ type BreakPointProviderProps = {
 
 const BreakpointProvider: FC<BreakPointProviderProps> = ({ children, breakPoints }) => {
   const [breakPointIndex, setBreakPointIndex] = useState(0);
-
+  
   useEffect(() => {
     const mediaQueryLists = [];
     let isAttached = false;
-
+    
     const handleQueryListener = () => {
       const updatedMatches = breakPoints.reduce((acc: number, media, index) => {
         acc = mediaQueryLists[index] && mediaQueryLists[index].matches ? index : acc;
         return acc;
       }, 0);
       setBreakPointIndex(updatedMatches);
+      
     };
-
+    
     if (window && window.matchMedia) {
       let matches = 0;
       breakPoints.forEach((bp, index) => {
         mediaQueryLists.push(
-          window.matchMedia(`screen and (min-width: ${bp}px)${breakPoints[index + 1] ? ` and (max-width: ${breakPoints[index + 1] - 1}px` : ''}`)
+            window.matchMedia(`screen and (min-width: ${bp}px)${breakPoints[index + 1]
+                                                                ? ` and (max-width: ${breakPoints[index + 1] - 1}px`
+                                                                : ""}`)
         );
         matches = mediaQueryLists[index].matches ? index : matches;
       });
@@ -67,7 +70,7 @@ const BreakpointProvider: FC<BreakPointProviderProps> = ({ children, breakPoints
         mediaQueryLists[index].addListener(handleQueryListener);
       });
     }
-
+    
     return () => {
       if (isAttached) {
         breakPoints.forEach((bp, index) => {
@@ -76,7 +79,7 @@ const BreakpointProvider: FC<BreakPointProviderProps> = ({ children, breakPoints
       }
     };
   }, [breakPoints]);
-
+  
   return <BreakpointContext.Provider value={breakPointIndex}>{children}</BreakpointContext.Provider>;
 };
 
@@ -86,36 +89,48 @@ type UseStyledSystemProps = {
 };
 
 const useStyledSystem = (
-  props: unknown,
-  { remBase = 10, fontSizes = [12, 14, 16, 20, 24, 32, 48, 64, 72], spaceSizes = [0, 4, 8, 16, 32, 64, 128, 256, 512], ...config }: ConfigProps
+    props: unknown,
+    {
+      remBase = 10, fontSizes = [12, 14, 16, 20, 24, 32, 48, 64, 72], spaceSizes = [
+      0,
+      4,
+      8,
+      16,
+      32,
+      64,
+      128,
+      256,
+      512
+    ], ...config
+    }: ConfigProps
 ): UseStyledSystemProps => {
   const breakPointIndex = React.useContext(BreakpointContext);
   const { cssProps, nonCssProps } = splitProps(props);
-
-  const [styleJsx, setStyleJsx] = useState<string>('');
-
+  
+  const [styleJsx, setStyleJsx] = useState<string>("");
+  
   useEffect(() => {
     setStyleJsx(
-      createStyledJsxStrings(cssProps, {
-        remBase,
-        fontSizes,
-        spaceSizes,
-        breakPointIndex,
-        ...config
-      })
+        createStyledJsxStrings(cssProps, {
+          remBase,
+          fontSizes,
+          spaceSizes,
+          breakPointIndex,
+          ...config
+        })
     );
   }, [breakPointIndex, config, cssProps, fontSizes, remBase, spaceSizes]);
-
+  
   return {
     styleJsx:
-      styleJsx ||
-      createStyledJsxStrings(props, {
-        remBase,
-        fontSizes,
-        spaceSizes,
-        breakPointIndex,
-        ...config
-      }),
+        styleJsx ||
+        createStyledJsxStrings(props, {
+          remBase,
+          fontSizes,
+          spaceSizes,
+          breakPointIndex,
+          ...config
+        }),
     nonCssProps
   };
 };
