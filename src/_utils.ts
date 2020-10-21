762cc4e04f3e6372f72b9a40461fda61276e39d9
FelixTellmann <felix@tellmann.co.za>
2020-10-21 20:35:22 +0200

    Refactoring

--- src/_utils.ts
+++ src/_utils.ts
@@ -171,7 +171,8 @@ export const createStyledJsxStrings = (props: unknown, { remBase, fontSizes, spa
     /* const result = postcss([autoprefixer]).process(toCssProperty(key, value)).then(({ css }) => css).toString(); */
     const prefixer = postcss.sync([autoprefixer]);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return toCssProperty(key, value);
+    /* return toCssProperty(key, value); */
+    console.log(prefixer(toCssProperty(key, value)))
     return Object.entries(prefixer(toCssProperty(key, value))).map(([key, value]) => {
       if (Array.isArray(value) && value.length > 1) {
         return value.map((val) => {
